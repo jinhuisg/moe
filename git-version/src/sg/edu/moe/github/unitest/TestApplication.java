d54dd03342ea84e26b5700e7f82f158ea9dab5ab
jinhuisg <jinhui.sg@gmail.com>
2015-07-12 11:50:51 +0800

    add new tag into comments.

--- git-version/src/sg/edu/moe/github/unitest/TestApplication.java
+++ git-version/src/sg/edu/moe/github/unitest/TestApplication.java
@@ -1,5 +1,12 @@
 package sg.edu.moe.github.unitest;
 
+/**
+ * It is Test programming to github usages.
+ * @author jinhui
+ * @since 2015
+ * @version 1.0
+ */
+
 public class TestApplication {
 
 	private final static String VERSION = "1.0";
