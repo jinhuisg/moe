3f231a255dadde38adfe429580df8983792a1abd
jinhuisg <jinhui.sg@gmail.com>
2015-07-12 11:55:22 +0800

    This is edited on github itself
    This is edited on github itself to simulate the changes done by other.

--- git-version/src/sg/edu/moe/github/unitest/TestApplication.java
+++ git-version/src/sg/edu/moe/github/unitest/TestApplication.java
@@ -9,11 +9,18 @@ package sg.edu.moe.github.unitest;
 
 public class TestApplication {
 
+	// This is for version control and it will be managed and synced with the tag in comment in header. 
 	private final static String VERSION = "1.0";
+	
 	
 	private final String AUTHOR = "Jin Hui";
 	
 	public static void main(String[] args) { 
 		System.out.println("Hello World, it is from MOE!");
 	}
+	
+	private void print(String value) { 
+		
+		System.out.println("Yes, the value is: " + value);
+	}	
 }
