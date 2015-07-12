c49447657bd70b7426b00bbd18f4b45550dea1c3
jinhuisg <jinhui.sg@gmail.com>
2015-07-12 12:07:05 +0800

    AnotherTest class is created for main application to invoke

---
+++ git-version/src/sg/edu/moe/github/unitest/AnotherTest.java
@@ -0,0 +1,23 @@
+package sg.edu.moe.github.unitest;
+
+public class AnotherTest {
+
+	private String VALUE = "Default Value";
+	
+	
+	
+	public AnotherTest() {
+		super();
+		//This is default constructor
+		
+		VALUE = "Value from constructor.";
+	}
+
+
+
+	public String getVALUE() {
+		return VALUE;
+	}
+
+
+}
