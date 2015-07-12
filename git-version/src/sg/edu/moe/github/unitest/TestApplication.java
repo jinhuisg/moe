b78f3b9c5abc00e4c034ac11dbc5c49f532a962d
jinhuisg <jinhui.sg@gmail.com>
2015-07-12 11:30:40 +0800

    Test class created for Git Hub usages
    It is to test out the github to help the team how it works.

---
+++ git-version/src/sg/edu/moe/github/unitest/TestApplication.java
@@ -0,0 +1,12 @@
+package sg.edu.moe.github.unitest;
+
+public class TestApplication {
+
+	private final static String VERSION = "1.0";
+	
+	private final String AUTHOR = "Jin Hui";
+	
+	public static void main(String[] args) { 
+		System.out.println("Hello World, it is from MOE!");
+	}
+}
